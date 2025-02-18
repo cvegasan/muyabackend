bae161ed5c8a17bb39bdbb348a96f3498a63d2d2
Cristian Alberto Vega Sanhueza <Cristian.Vega@sonda.com>
2025-02-18 19:03:14 -0300

    2025.02.18 Ajuste en Backend para actualizacion de favoritos, con perfil cliente

--- routes/favoritosRoutes.js
+++ routes/favoritosRoutes.js
@@ -10,10 +10,10 @@ router.get("/usuario/:user", favoritosController.getUserFavoritos);
 router.get("/producto/:prod", favoritosController.getProdFavoritos);
 router.post("/", favoritosController.postCrearFavoritos); //Clientes pueden agregar favoritos
 
-//INI ACCESO RESTRINGIDO con Token y Rol de admin (1)
+//INI ACCESO RESTRINGIDO con Token y Rol de admin (1) Cliente (2)
 router.put("/:id", verifyToken, verificarRol([1]),favoritosController.putActualizarFavoritos);
-router.delete("/:id", verifyToken, verificarRol([1]),favoritosController.deleteEliminarFavoritos);
-//FIN ACCESO RESTRINGIDO con Token y Rol de admin (1)
+router.delete("/:id", verifyToken, verificarRol([2]),favoritosController.deleteEliminarFavoritos);
+//FIN ACCESO RESTRINGIDO con Token y Rol de admin (1) Cliente (2)
 
 router.all('*', (req, res) => {
     res.status(404).json({ message: 'Pagina no encontrada' });
